380a7374484fd381895452a61e4fc8f303be5f3a
Arun Raj VK <arunvadekekara@gmail.com>
2022-06-02 23:47:47 +0530

    updated

--- qstns.js
+++ qstns.js
@@ -36,3 +36,41 @@ var twoSum = function(nums, target) {
         }
     }
  };
+
+
+ /* Write a javascript program to check two numbers
+ and return true if one of the number is 100 or if the sum
+ of the two number is 100*/
+
+
+ const get100 =(a,b) => a=== 100 || b=== 100 || (a+b)=== 100;
+  console.log(get100(100,0));
+  console.log(get100(0,100));
+
+  console.log(get100(30,34));
+  console.log(get100(60,13));
+
+  console.log(get100(30,70));
+  console.log(get100(60,40));
+
+
+  /* write a javascript program to get the extension
+  of a file name*/
+
+  const filename =(str) => str.slice
+  (str.lastIndexOf('.'));
+  console.log(filename('index.html'));
+  console.log(filename('javascript.index.js'));
+
+  /* write a javascript program to replace every character
+  in a given string with the character following
+  it in the alphabet*/
+
+  const moveCharsForvard = (str) =>
+     str
+     .split('')
+     .map(char => String.fromCharCode(char.charCodeAt (0) + 1))
+     .join('');
+     
+     
+     console.log(moveCharsForvard('abcdef'));
